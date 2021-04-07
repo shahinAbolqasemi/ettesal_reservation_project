d05ac1d8b7fb49feb3a005f0102fe184dac3b2ee
Shahin Abolqasemi <shahin.abolqasemi@yahoo.com>
2021-04-07 16:56:10 +0430

    add models participantAssignment and sessions_request

--- reservation_app/admin.py
+++ reservation_app/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 from .models import (
-    User,
+    User, Participant, SessionRequest, ParticipantAssignment
 )
+
 admin.site.register(User, ModelAdmin)
+admin.site.register(SessionRequest)
+admin.site.register(Participant)
+admin.site.register(ParticipantAssignment)
--- reservation_app/models.py
+++ reservation_app/models.py
@@ -81,8 +81,37 @@ class BaseModel(models.Model):
 
     created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
     modified_time = models.DateTimeField(verbose_name=_('modified time'), auto_now=True)
-    related_creator = models.ForeignKey(verbose_name=_('creator'), to=settings.AUTH_USER_MODEL,
-                                        on_delete=models.CASCADE)
+    related_creator = models.ForeignKey(
+        verbose_name=_('creator'),
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='%(class)s_creator_related',
+        related_query_name='%(class)s_creator'
+    )
+
+
+class SessionRequest(BaseModel):
+    nodes_count = models.PositiveIntegerField(verbose_name=_('nodes count'))
+    group_count = models.PositiveIntegerField(verbose_name=_('group count'))
+    related_customer = models.ForeignKey(
+        verbose_name=_('customer'),
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='%(class)s_customer_related',
+        related_query_name='%(class)s_customer'
+    )
+    is_accepted = models.BooleanField(verbose_name=_('is accepted'), default=False)
+    unique_id = models.CharField(verbose_name=_('unique id'), max_length=5, unique=True)
+    start_date = models.DateTimeField(verbose_name=_('start date'))
+    end_date = models.DateTimeField(verbose_name=_('end date'))
+    related_participants = models.ManyToManyField(
+        verbose_name=_('participants'),
+        to='Participant',
+        through='ParticipantAssignment',
+        through_fields=('related_session_request', 'related_participant'),
+        related_name='%(class)s_participants_related',
+        related_query_name='%(class)s_participants'
+    )
 
 
 class Participant(BaseModel):
@@ -90,4 +119,31 @@ class Participant(BaseModel):
 
     class Meta:
         verbose_name = _('participant')
-        verbose_name_plural = _('participant')
+        verbose_name_plural = _('participants')
+
+
+class ParticipantAssignment(BaseModel):
+    related_session_request = models.ForeignKey(
+        verbose_name=_('session request'),
+        to='SessionRequest',
+        on_delete=models.PROTECT,
+        related_name='%(class)s_session_request_related',
+        related_query_name='%(class)s_session_request'
+    )
+    related_participant = models.ForeignKey(
+        verbose_name=_('participant request'),
+        to='Participant',
+        on_delete=models.PROTECT,
+        related_name='%(class)s_participant_related',
+        related_query_name='%(class)s_participant'
+    )
+
+    class Meta:
+        verbose_name = _('participant assignment')
+        verbose_name_plural = _('participant assignments')
+        constraints = [
+            models.UniqueConstraint(
+                fields=('related_session_request', 'related_participant'),
+                name='unique_session_participant'
+            ),
+        ]
