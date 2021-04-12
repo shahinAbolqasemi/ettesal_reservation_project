3e79c37eb0b825251f0df1e158cade0ea9d5f3ff
Shahin Abolqasemi <shahin.abolqasemi@yahoo.com>
2021-04-12 15:57:40 +0430

    add IsAdminOrModifierSchedulerOrReadOnly permission and modify other permissions

--- reservation_app/permissions.py
+++ reservation_app/permissions.py
@@ -1,36 +1,56 @@
 from rest_framework import permissions
 from rest_framework.permissions import SAFE_METHODS
-from django.contrib.auth.models import Group
 
 
-class UserRoles:
-
-    @staticmethod
-    def is_scheduler(user):
-        try:
-            user.groups.get(name='scheduler')
-        except Group.DoesNotExist:
-            return False
-        return True
-
-
-class IsScheduler(permissions.BasePermission, UserRoles):
+class IsScheduler(permissions.BasePermission):
     """
     This permission is for check request.user that is scheduler
     """
 
     def has_permission(self, request, view):
-        return bool(request.user and self.is_scheduler(request.user))
+        return bool(request.user and request.user.check_group('scheduler'))
 
 
-class IsAdminOrReadOnly(permissions.BasePermission, UserRoles):
+class IsSchedulerOrCustomer(permissions.BasePermission):
     """
     This permission is for check request.user that is scheduler
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.check_any_groups(['customer', 'scheduler']))
+
+
+class IsAdminOrModifyOnly(permissions.BasePermission):
+    """
+    This permission is for check request.user that is admin.
+    if not can only modify retrieve objects.
+    customer can only change your self information
     """
 
     def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS or
             request.user and
-            request.user.is_superuser
+            request.user.is_superuser or
+            (request.user.check_group('scheduler') and request.method != 'POST') or
+            (request.user.check_group('customer') and request.method != 'POST')
+        )
+
+
+class IsAdminOrModifierSchedulerOrReadOnly(permissions.BasePermission):
+    """
+    This permission is for check request.user that is admin.
+    if is admin give full access
+    if scheduler give update access
+    if customer give view your self information
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            (
+                    request.user.is_superuser or
+                    (request.user.check_group('scheduler') and request.method != 'POST')
+            )
         )
