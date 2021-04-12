1a45e90a5be9b5eac1cebbad2ead92436ecae2ef
Shahin Abolqasemi <shahin.abolqasemi@yahoo.com>
2021-04-12 15:59:53 +0430

    Add SessionRequestViewSet and urls

--- reservation_app/urls.py
+++ reservation_app/urls.py
@@ -1,24 +1,10 @@
-"""reservation_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, SessionRequestViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='Users')
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'session-requests', SessionRequestViewSet, basename='session_request')
 
 urlpatterns = [
     path('', include(router.urls)),
--- reservation_app/views.py
+++ reservation_app/views.py
@@ -1,25 +1,89 @@
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
-from reservation_app.serializers import UserAdminSerializer, UserPublicSerializer
+from reservation_app.serializers import (
+    UserAdminSerializer,
+    UserCustomerSerializer,
+    SessionRequestSchedulerSerializer,
+    SessionRequestAdminSerializer, SessionRequestCustomerSerializer, UserSchedulerSerializer,
+)
 from . import permissions
 from rest_framework import permissions as base_permissions
+from .models import SessionRequest
 
 
 class UserViewSet(ModelViewSet):
     """
-    This ViewSet is view for List , Retrieve, Update and Delete (all) user
+    This ViewSet is for User model
     but with permissions
     """
-    serializer_class = UserPublicSerializer
+    serializer_class = UserCustomerSerializer
     queryset = get_user_model().objects.all()
     permission_classes = [
         base_permissions.IsAuthenticated,
-        permissions.IsScheduler,
-        permissions.IsAdminOrReadOnly
+        permissions.IsAdminOrModifyOnly,
     ]
 
     def get_serializer_class(self):
+        """
+        This method set serializer to:
+        UserAdminSerializer if user is admin
+        UserSchedulerSerializer if user is scheduler
+        else -> default serializer that is UserCustomerSerializer
+        """
         if self.request.user.is_superuser:
             return UserAdminSerializer
+        elif self.request.user.check_group('scheduler'):
+            return UserSchedulerSerializer
         return super().get_serializer_class()
 
+    def get_queryset(self):
+        """
+        This method return queryset.
+        if user is customer queryset limited to your self user account
+        else -> return default queryset that is (UserModel).objects.all()
+        """
+        if self.request.user.check_group('customer'):
+            return get_user_model().objects.filter(id=self.request.user.id)
+        return super().get_queryset()
+
+
+class SessionRequestViewSet(ModelViewSet):
+    """
+    The view is for SessionRequest model
+    """
+    serializer_class = SessionRequestAdminSerializer
+    queryset = SessionRequest.objects.all()
+    permission_classes = [
+        base_permissions.IsAuthenticated,
+        permissions.IsAdminOrModifierSchedulerOrReadOnly,
+    ]
+
+    def get_serializer_class(self):
+        """
+        This method return serializer class as follows:
+        if user is admin return SessionRequestAdminSerializer
+        if user is scheduler return SessionRequestSchedulerSerializer
+        if user is customer return SessionRequestCustomerSerializer
+        """
+        if self.request.user.is_superuser:
+            return SessionRequestAdminSerializer
+        elif self.request.user.check_group('scheduler'):
+            return SessionRequestSchedulerSerializer
+        elif self.request.user.check_group('customer'):
+            return SessionRequestCustomerSerializer
+
+    def perform_create(self, serializer):
+        """
+        This method is for add related_creator to data for perform create session request
+        Here set to applicant user
+        """
+        serializer.save(related_creator=self.request.user)
+
+    def get_queryset(self):
+        """
+        This method is for return queryset as follows:
+        if user is customer return your self session requests queryset
+        """
+        if self.request.user.check_group('customer'):
+            return SessionRequest.objects.filter(related_customer=self.request.user.id)
+        return super().get_queryset()
