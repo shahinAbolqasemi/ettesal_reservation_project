d10dc5362b289a7d3cc9cad26050bb5d7ba471ba
Shahin Abolqasemi <shahin.abolqasemi@yahoo.com>
2021-04-13 09:31:33 +0430

    add participant_assignments url and modify participants url

--- reservation_app/urls.py
+++ reservation_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, SessionRequestViewSet, ParticipantViewSet
+from .views import UserViewSet, SessionRequestViewSet, ParticipantViewSet, ParticipantAssignmentViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'session-requests', SessionRequestViewSet, basename='session_request')
-router.register(r'participants', ParticipantViewSet, basename='participant')
+router.register(r'participants', ParticipantViewSet, basename='participants')
+router.register(r'participant-assignments', ParticipantAssignmentViewSet, basename='participant_assignments')
 
 urlpatterns = [
     path('', include(router.urls)),
